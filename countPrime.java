f8e638c67dba39e3a393a7725dfe5ded6df0b1ab
Bahul Jain <bkj2111@columbia.edu>
2016-04-07 14:53:19 -0400

    adding a cooler way of doing this problem

--- countPrime.java
+++ countPrime.java
@@ -17,4 +17,21 @@ public class Solution {
    }
    return count;
     }
+    
+    // a sweet way of solving this problem using BitSets
+    public int countPrimes(int n) {
+        if(n<=2) return 0;
+        
+        BitSet checker = new BitSet(n);
+        checker.flip(0, n);
+        
+        int i = 2;
+        for(; i <= Math.sqrt(n); i=checker.nextSetBit(i+1)) {
+            for(int j=2; i*j < n; j++) {
+                checker.set(i*j, false);
+            }
+        }
+        
+        return checker.cardinality() - 2;
+    }
 }
